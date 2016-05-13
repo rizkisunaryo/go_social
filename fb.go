b2eef62292e4c9d0929485118404df354c221c75
Rizki Sunaryo <rizkisunaryo@gmail.com>
2016-05-13 07:49:03 +0700

    update

--- fb.go
+++ fb.go
@@ -64,3 +64,59 @@ func HttpHandleFbLogin(w http.ResponseWriter, r *http.Request) {
 
 	go_json.PrintJson(header,w,resp)
 }
+
+func HttpHandleFbLogin2() (
+string,
+func(string, http.ResponseWriter, *http.Request),
+func(string, http.ResponseWriter, *http.Request),
+func(string, http.ResponseWriter, *http.Request),
+func(string, http.ResponseWriter, *http.Request)) {
+
+	return "go_social.HttpHandleFbLogin", handleFbLogin, handleFbLogin, handleFbLogin, handleFbLogin
+}
+
+func handleFbLogin(header string, w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err!=nil {
+		log.Println(header, "1:", err.Error())
+		fmt.Fprintf(w, "{\"Message\":\""+strings.Replace(err.Error(),"\"","",-1)+"\"}")
+		return
+	}
+
+	log.Println(header,string(body))
+
+
+
+	var req model.FbLoginReq
+	json.Unmarshal(body, &req)
+
+	if req.Id=="" || req.Token=="" {
+		log.Println(header, "2")
+		fmt.Fprintf(w, "{\"Message\":\"incomplete request\"}")
+		return
+	}
+
+
+
+	var out model.FbLoginOut
+	body,err = go_http_request.GetInterface("https://graph.facebook.com/"+req.Id+"?access_token="+req.Token, &out, 30)
+	if err!=nil {
+		log.Println(header, "3", err.Error())
+		fmt.Fprintf(w, "{\"Message\":\""+strings.Replace(err.Error(),"\"","",-1)+"\"}")
+		return
+	}
+
+	var resp model.FbLoginResp
+	if out.Message!="" || out.Type!="" {
+		resp.Status="11"
+		resp.Message=out.Message
+		resp.Type=out.Type
+		resp.Code=out.Code
+	} else {
+		resp.Status="0"
+	}
+
+
+
+	go_json.PrintJson(header,w,resp)
+}
